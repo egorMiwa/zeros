a8dcda666fd8f99fc0bbb9531f308082fa7b045d
egorMiwa <36693566+egorMiwa@users.noreply.github.com>
2018-02-27 13:06:01 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var S = 0;
+  while (number >= 5)
+  {
+    number = Math.floor(number/5);
+    S = S + number;
+  }
+  return S;
 }
